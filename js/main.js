e90d5fd0d5b6c6982b959e5430f798aaeece3d93
Andrejb596 <andrejb596@gmail.com>
2022-09-26 10:48:11 +0300

    майже виконано

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(function () {
-    $('.popup__link').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
+	$('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
 
-    $('.gallery__slider').slick({
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/gallery__arr-left.svg" alt="arrow_left"></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/gallery__arr-right.svg" alt="arrow_right"></button>',
-    });
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/gallery__arr-left.svg" alt="arrow_left"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/gallery__arr-right.svg" alt="arrow_right"></button>',
+	});
 
-    $('.gallery__item-inner').magnificPopup({
+	$('.gallery__item-inner').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		closeOnContentClick: false,
@@ -25,10 +25,22 @@ $(function () {
 		zoom: {
 			enabled: true,
 			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
+			opener: function (element) {
 				return element.find('img');
 			}
 		}
-		
+
 	});
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active')
+	});
+
 });
+
+
+function color_changer(anything) {
+	document.querySelector('.header__title').style.color = anything;
+}
+
+
