5d45dd4bc2313a5cf5828c3f8ec8279c01a6be63
Andrejb596 <andrejb596@gmail.com>
2022-09-27 13:59:19 +0300

    змiни з iконками та перекладом сторiнки

---
+++ js/lang.js
@@ -0,0 +1,293 @@
+const lang_arr = {
+    'title_mane' : {
+        'en': 'Safari',
+        'ua': 'Сафарі',
+    },
+    'tel' : {
+        'en': 'Ordering tours and booking:',
+        'ua': 'Замовлення та бронювання турів:',
+    },
+    'booking' : {
+        'en': 'Booking',
+        'ua': 'Бронювання',
+    },
+    'excursion' : {
+        'en': 'Excursions',
+        'ua': 'Екскурсії',
+    },
+    'tours' : {
+        'en': 'Tours',
+        'ua': 'Тури',
+    },
+    'about__af' : {
+        'en': 'About Africa',
+        'ua': 'Про Африку',
+    },
+    'about__us' : {
+        'en': 'About Us',
+        'ua': 'Про нас',
+    },
+    
+    'header__title' : {
+        'en': 'Safari in Africa',
+        'ua': 'Сафарі в Африці',
+    },
+    
+    'header__descr' : {
+        'en': 'Tours to the most exotic places Come with us for new experiences!',
+        'ua': 'Тури в найекзотичніші місця. Приїжджайте з нами за новими враженнями!',
+    },
+    
+    'header__btn' : {
+        'en': 'Submit your application',
+        'ua': 'Надішліть заявку',
+    },
+    // direction
+    'Namibia' : {
+        'en': 'Namibia',
+        'ua': 'Намібія',
+    },
+    'Tanzania' : {
+        'en': 'Tanzania',
+        'ua': 'Танзанія',
+    },
+    'Kenya' : {
+        'en': 'Kenya',
+        'ua': 'Кенія',
+    },
+    'Cape__town' : {
+        'en': 'Cape Town',
+        'ua': 'Кейптаун',
+    },
+    'Madagascar' : {
+        'en': 'Madagascar',
+        'ua': 'Мадагаскар',
+    },
+    'Zanzibar' : {
+        'en': 'Zanzibar',
+        'ua': 'Занзібар',
+    },
+    'Mozambique' : {
+        'en': 'Mozambique',
+        'ua': 'Мозамбік',
+    },
+    'Zambia' : {
+        'en': 'Zambia',
+        'ua': 'Замбія',
+    },
+    'Congo' : {
+        'en': 'Congo',
+        'ua': 'Конго',
+    },
+    'Botswana' : {
+        'en': 'Botswana',
+        'ua': 'Ботсвана',
+    },
+    'South__africa' : {
+        'en': 'South Africa',
+        'ua': 'Південна Африка',
+    },
+    // travel__info
+    'traver__title' : {
+        'en': 'Main directions',
+        'ua': 'Основні напрями',
+    },
+    'traver__text-1' : {
+        'en': 'Full organization of recreation: group and individual tours.',
+        'ua': 'Повна організація відпочинку: групові та індивідуальні тури.',
+    },
+    'traver__text-2' : {
+        'en': 'Organization of a transfer from anywhere in the world',
+        'ua': 'Організація трансферу з будь-якої точки світу',
+    },
+    'traver__text-3' : {
+        'en': 'Accommodation in hotels along the route',
+        'ua': 'Розміщення в готелях по маршруту',
+    },
+    'traver__text-4' : {
+        'en': 'Car rental and any type of transport',
+        'ua': 'Оренда авто та будь-якого виду транспорту',
+    },
+    'traver__text-5' : {
+        'en': 'Medical insurance',
+        'ua': 'Медичне страхування',
+    },
+    'traver__text-6' : {
+        'en': 'Accompanying Russian-speaking guides',
+        'ua': 'Супровід російськомовних гідів',
+    },
+    'traver__text-7' : {
+        'en': 'Organization of VIP tours',
+        'ua': 'Організація VIP турів',
+    },
+    // popular
+    'popular__price' : {
+        'en': 'from',
+        'ua': 'від',
+    },
+    // доделать для всіх 'from'
+
+    'popular__card-name__1' : {
+        'en': 'JOURNEY IN NAMIBIA',
+        'ua': 'ПОДОРОЖ НАМІБІЄЮ',
+    },
+    'popular__card-name__2' : {
+        'en': 'KENYA CLASSICS',
+        'ua': 'КЕНІЙСЬКА КЛАСИКА',
+    },
+    'popular__card-name__3' : {
+        'en': 'THE MOST POPULAR SAFARI PARKS AND RESERVES IN TANZANIA',
+        'ua': 'НАЙПОПУЛЯРНІШІ САФАРІ-ПАРКІ ТА ЗАПОВІДНИКИ ТАНЗАНІЇ',
+    },
+    'popular__card-p__3' : {
+        'en': 'Tanzania',
+        'ua': 'Танзанія',
+    },
+    'popular__card-day' : {
+        'en': 'days',
+        'ua': 'днів',
+    },
+    'popular__card-night' : {
+        'en': 'nights',
+        'ua': 'ночей',
+    },
+    'popular__card-name__4' : {
+        'en': 'BOTSWANA STUDY',
+        'ua': 'ДОСЛІДЖЕННЯ БОТСВАНИ',
+    },
+    'popular__card-name__5' : {
+        'en': 'JOURNEY THROUGH 5 COUNTRIES OF SOUTH AFRICA',
+        'ua': 'ПОДОРОЖ 5 КРАЇНАМИ ПІВДЕННОЇ АФРИКИ',
+    },
+    'popular__card-name__6' : {
+        'en': 'GRAND ADVENTURE IN 9 AFRICA COUNTRIES',
+        'ua': 'ГРАНДІЗНА ПРИГОДА В 9 КРАЇНАХ АФРИКИ',
+    },
+    'popular__card-p__6' : {
+        'en': 'South Africa, Zambia, Namibia, Zimbabwe, Botswana, Mozambique, Tanzania, Kenya',
+        'ua': 'Південна Африка, Замбія, Намібія, Зімбабве, Ботсвана, Мозамбік, Танзанія, Кенія',
+    },
+    'popular__card-name__7' : {
+        'en': 'GREAT CAPE TOWN',
+        'ua': 'ВЕЛИКИЙ КЕЙПТАУН',
+    },
+    'popular__card-name__8' : {
+        'en': 'JOURNEY IN SOUTH AFRICA:',
+        'ua': 'ПОДОРОЖ ПІВДЕННОЮ АФРИКОЮ:',
+    },
+    'popular__card-name__9' : {
+        'en': 'THE GREAT MIGRATION IN TANZANIA AND HOLIDAYS IN ZANZIBAR',
+        'ua': 'ВЕЛИКА МІГРАЦІЯ В ТАНЗАНІЇ ТА ВІДПОЧИНОК НА ЗАНЗІБАРІ',
+    },
+    'popular__card-name__10' : {
+        'en': 'FROM CAPE TOWN TO VICTORIA FALLS',
+        'ua': 'ВІД КЕЙПТАУНА ДО ВОДОСПАДУ ВІКТОРІЯ',
+    },
+    // map
+    'map__title' : {
+        'en': 'Africa map',
+        'ua': 'Карта Африки',
+    },
+    // about
+    'about__title' : {
+        'en': 'About safari tours',
+        'ua': 'Про сафарі-тури',
+    },
+    'about__text' : {
+        'en': 'East and South Africa are the most popular places where you can enjoy an exciting journey called a safari. It is here that all conditions are created so that tourists can feel like a piece of the virgin nature of Africa. Safari tours to Africa are characterized by unique programs in which tourists move in comfortable, specialized cars or buses. East and South Africa are the most popular places where you can enjoy an exciting journey called a safari. It is here that all conditions are created so that tourists can feel like a piece of the virgin nature of Africa.',
+        'ua': 'Східна та Південна Африка є найпопулярнішими місцями, де можна насолодитися захоплюючим подорож називається сафарі. Саме тут створені всі умови для того, щоб туристи могли почуватися як єдине ціле незайманої природи Африки. Сафарі-тури в Африку характеризуються унікальними програмами, в яких туристи пересуваються на комфортабельних, спеціалізованих автомобілях або автобусах. Східна та Південна Африка – найпопулярніші місця де ви зможете насолодитися захоплюючою подорожжю під назвою сафарі. Саме тут створені всі умови для того туристи можуть відчути себе шматочком незайманої природи Африки.',
+    },
+    'about__link' : {
+        'en': 'Read more',
+        'ua': 'Читати далі',
+    },
+    // gallery
+    'gallery__title' : {
+        'en': 'Photo gallery',
+        'ua': 'Фотогалерея',
+    },
+    'gallery__link' : {
+        'en': 'Go to section',
+        'ua': 'Перейти до розділу',
+    },
+    // tour
+    'tour__title' : {
+        'en': 'Premium safari tour',
+        'ua': 'Преміальний сафарі-тур',
+    },
+    'tour__text-1' : {
+        'en': 'Safari tour? this is a unique journey into the world of wildlife, acquaintance with the many-sided Africa, observation of wild animals in their usual habitat.',
+        'ua': 'Сафарі-тур? це унікальна подорож у світ живої природи, знайомство з багатолика Африка, спостереження за дикими тваринами в їх звичному середовищі проживання.',
+    },
+    'tour__text-span-2' : {
+        'en': 'TOURIST SAFARI',
+        'ua': 'ТУРИСТИЧНЕ САФАРИ',
+    },
+    'tour__text-2' : {
+        'en': 'specializes in luxury tourism and presents its clients with unique programs of safari tours. Among the options presented, you can find something unique, according to your taste.',
+        'ua': 'спеціалізується на туризмі класу люкс і представляє своїм клієнтам унікальні програми сафарі-турів. Серед представлених варіантів можна знайти щось унікальне, на свій смак.',
+    },
+    'tour__text-3' : {
+        'en': 'If you want to go on the most unforgettable journey of your life, visit unique places and see what few people manage to see? then safari tours are made for you.',
+        'ua': 'Якщо ви хочете відправитися в найнезабутнішу подорож свого життя, відвідайте унікальні місця і побачити те, що мало кому вдається побачити? тоді сафарі-тури створені для вас.',
+    },
+    'tour__text-4' : {
+        'en': 'Safari tours are chosen by tourists who prefer a dynamic, bright, full of emotions and impressions vacation. Africa safari tour programs are designed for a specific group of people who, under the auspices of a curator, carry out an exciting journey along a pre-planned route. We are focused on VIP class clients, so all the offers presented in our collection are distinguished by the highest class, a well-thought-out program, first-class service and an individual approach to each travel participant. On our website you can read all the details about the programs of already formed safari tours.',
+        'ua': 'Сафарі-тури обирають туристи, які віддають перевагу динамічному, яскравому, насиченому емоціями та враженнями відпочинку. Програми сафарі-турів по Африці розраховані на певну групу людей, які під егідою куратора здійснити захоплюючу подорож за заздалегідь спланованим маршрутом. Ми орієнтовані на клієнтів VIP класу, тому всі пропозиції, представлені в нашій колекції, відрізняє найвищий клас, продумана програма, першокласний сервіс та індивідуальний підхід до кожного учасника подорожі. На нашому сайті ви можете прочитати все детально про програми вже сформованих сафарі-турів.',
+    },
+    'tour__info-text-1' : {
+        'en': 'years of successful work',
+        'ua': 'років успішної роботи',
+    },
+    'tour__info-text-2' : {
+        'en': 'hotels without intermediaries',
+        'ua': 'готелі без посередників',
+    },
+    'tour__info-text-3' : {
+        'en': 'own tours',
+        'ua': 'власні тури',
+    },
+    'tour__info-text-4' : {
+        'en': 'sq. km - the area of our travels',
+        'ua': 'кв.км - територія наших подорожей',
+    },
+    'tour__info-text-5' : {
+        'en': 'satisfied tourists',
+        'ua': 'задоволені туристи',
+    },
+    // booking
+    'booking__title' : {
+        'en': 'Book your holiday in Africa',
+        'ua': 'Забронюйте відпустку в Африці',
+    },
+    'booking__btn' : {
+        'en': 'Submit an application',
+        'ua': 'Подайте заявку',
+    },
+    'booking__text' : {
+        'en': 'Consent to the processing of personal data',
+        'ua': 'Згода на обробку персональних даних',
+    },
+    //footer
+    'footer___title-1' : {
+        'en': 'Travel agency "Safari Touristik"',
+        'ua': 'Турагенція "Сафарі Турист"',
+    },
+    'footer___title-2' : {
+        'en': 'Address:',
+        'ua': 'Адреса:',
+    },
+    'footer___title-3' : {
+        'en': 'Ordering tours and booking:',
+        'ua': 'Замовлення та бронювання турів:',
+    },
+    'address' : {
+        'en': 'Kyiv, Mira street, 20, Dynamo Hotel, 1st floor',
+        'ua': 'Київ, вул. Миру, 20, готель «Динамо», 1 поверх',
+    },
+    'footer__link' : {
+        'en': 'Privacy Policy',
+        'ua': 'Політика конфіденційності',
+    },
+
+};
--- js/main.js
+++ js/main.js
@@ -43,4 +43,55 @@ function color_changer(anything) {
 	document.querySelector('.header__title').style.color = anything;
 }
 
+// for language 
 
+const select = document.querySelector('select');
+const all_lang = ['en', 'ru', 'ua'];
+
+select.addEventListener('change', change_URL_language);
+
+// перенаправить на url с указанием языка
+function change_URL_language() {
+	let lang = select.value;
+	location.href = window.location.pathname + '#' + lang;
+	location.reload();
+}
+
+function change_language() {
+	let hash = window.location.hash;
+	hash = hash.substr(1);
+	console.log(hash);
+	if (!all_lang.includes(hash)) {
+		location.href = window.location.pathname + '#en';
+		location.reload();
+	}
+	select.value = hash;
+	document.querySelector('title').innerHTML = lang_arr['title_mane'][hash];
+	// document.querySelector('.lng-chip').innerHTML = lang_arr['chip'][hash]; // для одного
+	for (let key in lang_arr) {
+		let elem = document.querySelector('.lng-' + key);
+		if (elem) {
+			elem.innerHTML = lang_arr[key][hash];
+		}
+
+	}
+	// переробить
+	const lng_night = document.querySelectorAll('.lng-popular__card-night');
+    for (let i = 0; i < lng_night.length; i++) {
+        lng_night[i].innerHTML = lang_arr['popular__card-night'][hash];
+    }
+
+	const lng_day = document.querySelectorAll('.lng-popular__card-day');
+    for (let i = 0; i < lng_day.length; i++) {
+        lng_day[i].innerHTML = lang_arr['popular__card-day'][hash];
+    }
+
+	const lng_price = document.querySelectorAll('.lng-popular__price');
+    for (let i = 0; i < lng_price.length; i++) {
+        lng_price[i].innerHTML = lang_arr['popular__price'][hash];
+    }
+
+
+}
+
+change_language();
