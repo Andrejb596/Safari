0ab9e292886fb1c39bfdaad128697d39b05f29f4
Andrejb596 <andrejb596@gmail.com>
2022-09-28 22:46:51 +0300

    удосконалив перевод сторiнки

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function color_changer(anything) {
 // for language 
 
 const select = document.querySelector('select');
-const all_lang = ['en', 'ru', 'ua'];
+const all_lang = ['en', 'ua'];
 
 select.addEventListener('change', change_URL_language);
 
@@ -60,38 +60,20 @@ function change_URL_language() {
 function change_language() {
 	let hash = window.location.hash;
 	hash = hash.substr(1);
-	console.log(hash);
 	if (!all_lang.includes(hash)) {
 		location.href = window.location.pathname + '#en';
 		location.reload();
 	}
 	select.value = hash;
 	document.querySelector('title').innerHTML = lang_arr['title_mane'][hash];
-	// document.querySelector('.lng-chip').innerHTML = lang_arr['chip'][hash]; // для одного
 	for (let key in lang_arr) {
-		let elem = document.querySelector('.lng-' + key);
-		if (elem) {
-			elem.innerHTML = lang_arr[key][hash];
+		let elem = document.querySelectorAll('.lng-' + key);
+		for (let i = 0; i < elem.length; i++) {
+			if (elem) {
+				elem[i].innerHTML = lang_arr[key][hash];
+			}
 		}
-
 	}
-	// переробить
-	const lng_night = document.querySelectorAll('.lng-popular__card-night');
-    for (let i = 0; i < lng_night.length; i++) {
-        lng_night[i].innerHTML = lang_arr['popular__card-night'][hash];
-    }
-
-	const lng_day = document.querySelectorAll('.lng-popular__card-day');
-    for (let i = 0; i < lng_day.length; i++) {
-        lng_day[i].innerHTML = lang_arr['popular__card-day'][hash];
-    }
-
-	const lng_price = document.querySelectorAll('.lng-popular__price');
-    for (let i = 0; i < lng_price.length; i++) {
-        lng_price[i].innerHTML = lang_arr['popular__price'][hash];
-    }
-
-
 }
 
 change_language();
